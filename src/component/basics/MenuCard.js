ab1946c20d2cc822cc1e8fc2502efbef2489b2c2
arjun <arjun@webmobinfo.ch>
2022-10-03 16:40:27 +0530

    resto react project

---
+++ src/component/basics/MenuCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+// In React 17 React import is bydefault work & we cannot use this but some module required this import 
+// this is faterror function & it is used for call a function and return is must
+const MenuCard = ({ menuData }) => {
+  //   console.log(menuData);
+
+  return (
+    <>
+      <section className="main-card--cointainer">
+        {menuData.map((curElem) => {
+          const { id, name, category, image, description } = curElem;
+
+          return (
+            <>
+            {/* This is div for one component meggi  & we can use this by using usestate function  */}
+              <div className="card-container" key={id}>
+                <div className="card ">
+                  <div className="card-body">
+                    <span className="card-number card-circle subtle">{id}</span>
+                    <span className="card-author subtle"> {category}</span>
+                    <h2 className="card-title"> {name} </h2>
+                    <span className="card-description subtle">
+                      {description}
+                    </span>
+                    <div className="card-read">Read</div>
+                  </div>
+                  <img src={image} alt="images" className="card-media" />
+
+                  <span className="card-tag  subtle">Order Now</span>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </section>
+    </>
+  );
+};
+
+export default MenuCard;
